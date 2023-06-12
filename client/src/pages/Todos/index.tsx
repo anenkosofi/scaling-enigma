f17690127d3cf47c3670791545ab99ca46c9ce1f
Sofiia Anenko <anenko.sofiia@gmail.com>
2023-06-12 22:33:07 +0300

    fix: add get all todos

--- client/src/pages/Todos/index.tsx
+++ client/src/pages/Todos/index.tsx
@@ -1,12 +1,19 @@
-import React, { FC, useContext } from 'react';
+import React, { FC, useContext, useEffect } from 'react';
 
 import { ThemeContext } from '@components/ThemeProvider';
 import { ThemeToggler } from '@components/ThemeToggler';
 import { TodoForm } from '@components/TodoForm';
 import { TodoList } from '@components/TodoList';
+import { useAppDispatch } from '@hooks';
+import { getTodos } from '@store/todos/operations';
 
 const TodosPage: FC = () => {
+  const dispatch = useAppDispatch();
   const { theme } = useContext(ThemeContext);
+
+  useEffect(() => {
+    dispatch(getTodos());
+  }, []);
 
   return (
     <div className={`theme-${theme}`}>
