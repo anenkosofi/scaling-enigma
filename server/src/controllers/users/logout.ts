759622de3e4ef01992daeb0010c397152b3171b7
Sofiia Anenko <anenko.sofiia@gmail.com>
2023-06-13 19:28:34 +0300

    fix: remove logout controller

--- server/src/controllers/users/logout.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-import { User } from '@models';
-
-export const logout = async (req: Request, res: Response) => {
-  const { _id } = req.user;
-  await User.findByIdAndUpdate(_id, { token: null });
-  res.status(204).json();
-};
--- server/src/routes/api/users.ts
+++ server/src/routes/api/users.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { login, register, getCurrent, logout } from '@controllers/users';
+import { login, register, getCurrent } from '@controllers/users';
 import { validateBody, auth } from '@middlewares';
 import { userSchemas } from '@models';
 
@@ -15,5 +15,3 @@ usersRouter.post(
 usersRouter.post('/login', validateBody(userSchemas.userLoginJoiSchema), login);
 
 usersRouter.get('/current', auth, getCurrent);
-
-usersRouter.post('/logout', auth, logout);
