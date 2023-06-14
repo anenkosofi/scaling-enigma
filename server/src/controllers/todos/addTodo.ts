bfe566d0912caa2e7727b4a6fb49771cae14e3a1
Sofiia Anenko <anenko.sofiia@gmail.com>
2023-06-14 10:22:02 +0300

    fix: edit todo response

--- server/src/controllers/todos/addTodo.ts
+++ server/src/controllers/todos/addTodo.ts
@@ -5,7 +5,13 @@ import { Todo } from '@models';
 export const addTodo = async (req: Request, res: Response) => {
   const { _id } = req.user;
   const newTodo = await Todo.create({ ...req.body, owner: _id });
+  const { _id: todoId, text, completed, time } = newTodo;
   res.status(201).json({
-    todo: newTodo,
+    todo: {
+      _id: todoId,
+      text,
+      completed,
+      time,
+    },
   });
 };
