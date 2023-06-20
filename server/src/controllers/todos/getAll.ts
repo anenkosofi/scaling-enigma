ebba214f08d044384de48962e89d4d47d447c200
Sofiia Anenko <anenko.sofiia@gmail.com>
2023-06-20 10:46:10 +0300

    feat: update get all controller

--- server/src/controllers/todos/getAll.ts
+++ server/src/controllers/todos/getAll.ts
@@ -7,13 +7,25 @@ interface UserRequest extends Request {
   user: {
     _id: string;
   };
+  query: {
+    query: string;
+  };
 }
+
+type Params = {
+  owner: string;
+  query?: string;
+};
 
 export const getAll = controller(async (req: UserRequest, res: Response) => {
   const { _id } = req.user;
-  const query = {
+  const { query } = req.query;
+  const params: Params = {
     owner: _id,
   };
-  const todos = await Todo.find(query).select('-owner');
+  if (query) {
+    params.query = query;
+  }
+  const todos = await Todo.find(params).select('-owner');
   res.json(todos);
 });
