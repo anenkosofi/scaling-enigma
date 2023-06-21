823dedace14f4733bd98afcdf5cdfc253f9b35cd
Sofiia Anenko <anenko.sofiia@gmail.com>
2023-06-21 15:05:36 +0300

    fix: add onemore dependency

--- client/src/pages/Todos/index.tsx
+++ client/src/pages/Todos/index.tsx
@@ -7,16 +7,20 @@ import { TodoList } from '@components/TodoList';
 import { useAppDispatch, useAppSelector } from '@hooks';
 import { selectQuery, selectStatusFilter } from '@store/filters/selectors';
 import { getTodos } from '@store/todos/operations';
+import { selectTodos } from '@store/todos/selectors';
 
 const TodosPage: FC = () => {
   const dispatch = useAppDispatch();
   const { theme } = useContext(ThemeContext);
+  const todos = useAppSelector(selectTodos);
   const query = useAppSelector(selectQuery);
   const status = useAppSelector(selectStatusFilter);
 
+  const completedTasks = todos.filter(todo => todo.completed);
+
   useEffect(() => {
     dispatch(getTodos());
-  }, [query, status]);
+  }, [query, status, completedTasks.length]);
 
   return (
     <div className={`theme-${theme}`}>
