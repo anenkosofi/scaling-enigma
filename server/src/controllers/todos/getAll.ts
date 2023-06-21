3f9a22373a4bef9767cbede2f6b1aaf863dbb62c
Sofiia Anenko <anenko.sofiia@gmail.com>
2023-06-21 13:50:23 +0300

    feat: change get todos controller

--- server/src/controllers/todos/getAll.ts
+++ server/src/controllers/todos/getAll.ts
@@ -1,27 +1,35 @@
 import { Request, Response } from 'express';
+import { ObjectId } from 'mongoose';
 
 import { Todo } from '@models';
 import { controller } from '@utils';
+import { Query, User } from '@types';
 
-interface UserRequest extends Request {
-  user: {
-    _id: string;
-  };
-  query: {
-    query: string;
-  };
-}
+type Params = {
+  owner: ObjectId;
+  text?: { $regex: RegExp };
+  completed?: boolean;
+};
 
-export const getAll = controller(async (req: UserRequest, res: Response) => {
-  const { _id } = req.user;
-  const { query } = req.query;
+export const getAll = controller(async (req: Request, res: Response) => {
+  const { _id } = req.user as User;
+  const { query, completed } = req.query as Query;
+
+  const params: Params = {
+    owner: _id,
+  };
+
+  if (query) {
+    params.text = { $regex: new RegExp(query, 'i') };
+  }
+
+  if (typeof completed === 'boolean') {
+    params.completed = completed;
+  }
 
   const todos = await Todo.aggregate([
     {
-      $match: {
-        text: { $regex: new RegExp(query, 'i') },
-        owner: _id,
-      },
+      $match: params,
     },
     {
       $project: {
